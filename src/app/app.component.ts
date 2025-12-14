485714a9ac270b5e6defa1046e1ae48f7a148d5f
rajeshkumar1635 <rajeshkumar.kurapati23@gmail.com>
2025-12-14 16:04:44 -0500

    added and created the services

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { ProductsListComponent } from "./pages/products-list/products-list.component";
+@Component({
+  selector: 'app-root',
+  imports: [HeaderComponent, ProductsListComponent],
+  template: `
+    <app-header></app-header>
+    <app-products-list/>
+  `,
+  styles: ` `,
+})
+export class AppComponent {
+  title = 'angular-ecomm';
+}
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,22 @@
+import { Component,inject,signal } from '@angular/core';
+import { PrimaryButtonComponent } from "../primary-button/primary-button.component";
+import { CartService } from '../../services/cart.service';
+
+@Component({
+  selector: 'app-header',
+  imports: [PrimaryButtonComponent],
+  template: `
+    <div class="bg-slate-100 px-4 py-3 shadow-md flex justify-between items-center">
+      <span class="text-xl">My Store</span>
+      <app-primary-button [label]="'Cart ('+ cartService.cart().length+')'"/>
+</div>
+  `,
+  styles: `
+  `
+})
+export class HeaderComponent {
+
+  cartService= inject(CartService);
+
+
+}
